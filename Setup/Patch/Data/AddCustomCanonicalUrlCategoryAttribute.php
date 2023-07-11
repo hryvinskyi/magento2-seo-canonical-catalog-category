1c352793521f36e460669685e46f60a2dd7cf3e9
Volodymyr Hryvinskyi <volodymyr@hryvinskyi.com>
2023-07-11 18:34:10 +0300

    first commit

---
+++ Setup/Patch/Data/AddCustomCanonicalUrlCategoryAttribute.php
@@ -0,0 +1,79 @@
+<?php
+/**
+ * Copyright (c) 2023. MageCloud.  All rights reserved.
+ * @author: Volodymyr Hryvinskyi <mailto:volodymyr@hryvinskyi.com>
+ */
+
+namespace Hryvinskyi\SeoCanonicalCatalogCategory\Setup\Patch\Data;
+
+use Magento\Catalog\Model\Category;
+use Magento\Eav\Model\Entity\Attribute\ScopedAttributeInterface;
+use Magento\Eav\Setup\EavSetup;
+use Magento\Eav\Setup\EavSetupFactory;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+use Magento\Framework\Setup\Patch\DataPatchInterface;
+
+class AddCustomCanonicalUrlCategoryAttribute implements DataPatchInterface
+{
+    public function __construct(
+        private readonly ModuleDataSetupInterface $moduleDataSetup,
+        private readonly EavSetupFactory $eavSetupFactory
+    ) {
+    }
+
+    /**
+     * Run code inside patch
+     * If code fails, patch must be reverted, in case when we are speaking about schema - then under revert
+     * means run PatchInterface::revert()
+     *
+     * If we speak about data, under revert means: $transaction->rollback()
+     */
+    public function apply(): void
+    {
+        /** @var EavSetup $eavSetup */
+        $eavSetup = $this->eavSetupFactory->create(['setup' => $this->moduleDataSetup]);
+
+        $eavSetup->addAttribute(
+            Category::ENTITY,
+            'custom_canonical_url',
+            [
+                'input' => 'text',
+                'visible' => true,
+                'global' => ScopedAttributeInterface::SCOPE_STORE,
+                'label' => 'Custom Canonical URL',
+                'source' => null,
+                'type' => 'varchar',
+                'sort_order' => 100,
+                'required' => false,
+                'group' => 'Search Engine Optimization',
+            ]
+        );
+    }
+
+    /**
+     * Get array of patches that have to be executed prior to this.
+     *
+     * Example of implementation:
+     *
+     * [
+     *      \Vendor_Name\Module_Name\Setup\Patch\Patch1::class,
+     *      \Vendor_Name\Module_Name\Setup\Patch\Patch2::class
+     * ]
+     *
+     * @return string[]
+     */
+    public static function getDependencies(): array
+    {
+        return [];
+    }
+
+    /**
+     * Get aliases (previous names) for the patch.
+     *
+     * @return string[]
+     */
+    public function getAliases(): array
+    {
+        return [];
+    }
+}
